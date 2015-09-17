5e0bd63ba496948c07adc6f62cfcbf379d49bb03
bgreer5050 <bgreer5050@gmail.com>
2015-09-17 08:07:46 -0500

    Added Slider CodeBehind - NOT WORKING

--- ZumbachPi_V2.0/MainPage.xaml.cs
+++ ZumbachPi_V2.0/MainPage.xaml.cs
@@ -24,22 +24,28 @@ namespace ZumbachPi_V2._0
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int intSliderValue;
+        public string strZoomLevel;
+
         public MainPage()
         {
             this.InitializeComponent();
-
+            intSliderValue = 1;
+           
             this.Loaded += MainPage_Loaded;
         }
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            LoadWebView();
+          
             Debug.WriteLine(this.Width.ToString());
             Debug.WriteLine(this.Height.ToString());
+
+            
         }
 
 
-        private void LoadWebView()
+        private void LoadWebView(int _zoom)
         {
            
 
@@ -48,14 +54,101 @@ namespace ZumbachPi_V2._0
             sb.Append(@"<head>");
             sb.Append(@"<meta charset=""utf - 8"" />");
             sb.Append(@" <title></title>");
+
+
+            switch(intSliderValue)
+            {
+                case 1:
+                    sb.Append(@"<style>
+                              #mybody {
+                            -ms-zoom: 0.75;
+                            -moz-transform: scale(0.75);
+                            -moz-transform-origin: 0 0;
+                            -o-transform: scale(0.75);
+                            -o-transform-origin: 0 0;
+                            -webkit-transform: scale(0.75);
+                            -webkit-transform-origin: 0 0;
+                        }
+                    </style>");
+                    break;
+
+                case 2:
+                    sb.Append(@"<style>
+                              #mybody {
+                            -ms-zoom: 1.00;
+                            -moz-transform: scale(1.00);
+                            -moz-transform-origin: 0 0;
+                            -o-transform: scale(1.00);
+                            -o-transform-origin: 0 0;
+                            -webkit-transform: scale(1.00);
+                            -webkit-transform-origin: 0 0;
+                        }
+                    </style>");
+                    break;
+
+                case 3:
+                    sb.Append(@"<style>
+                              #mybody {
+                            -ms-zoom: 1.50;
+                            -moz-transform: scale(1.50);
+                            -moz-transform-origin: 0 0;
+                            -o-transform: scale(1.50);
+                            -o-transform-origin: 0 0;
+                            -webkit-transform: scale(1.50);
+                            -webkit-transform-origin: 0 0;
+                        }
+                    </style>");
+                    break;
+
+                case 4:
+                    sb.Append(@"<style>
+                              #mybody {
+                            -ms-zoom: 2.00;
+                            -moz-transform: scale(2.00);
+                            -moz-transform-origin: 0 0;
+                            -o-transform: scale(2.00);
+                            -o-transform-origin: 0 0;
+                            -webkit-transform: scale(2.00);
+                            -webkit-transform-origin: 0 0;
+                        }
+                    </style>");
+                    break;
+
+                default:
+                    sb.Append(@"<style>
+                                #mybody {
+                              -ms-zoom: 1.00;
+                              -moz-transform: scale(1.00);
+                              -moz-transform-origin: 0 0;
+                              -o-transform: scale(1.00);
+                              -o-transform-origin: 0 0;
+                              -webkit-transform: scale(1.00);
+                              -webkit-transform-origin: 0 0;
+                          }
+                      </style>");
+                    break;
+            }
+
+            //        sb.Append(@"<style>
+            //          #mybody {
+            //        -ms-zoom: 1.50;
+            //        -moz-transform: scale(1.50);
+            //        -moz-transform-origin: 0 0;
+            //        -o-transform: scale(1.50);
+            //        -o-transform-origin: 0 0;
+            //        -webkit-transform: scale(1.50);
+            //        -webkit-transform-origin: 0 0;
+            //    }
+            //</style>");
+
             sb.Append(@"<style>
-        #mybody {
-            -ms-zoom: 1.40;
-            -moz-transform: scale(1.40);
+              #mybody {
+            -ms-zoom: 1.00;
+            -moz-transform: scale(1.00);
             -moz-transform-origin: 0 0;
-            -o-transform: scale(1.40);
+            -o-transform: scale(1.00);
             -o-transform-origin: 0 0;
-            -webkit-transform: scale(1.40);
+            -webkit-transform: scale(1.00);
             -webkit-transform-origin: 0 0;
         }
     </style>");
@@ -67,12 +160,12 @@ namespace ZumbachPi_V2._0
 
             sb.Append(@"</head>");
             sb.Append(@"<body id=""mybody"" style=""margin: 0px; padding: 0px; overflow: hidden; "">");
-            sb.Append(@" <iframe id=""myiframe"" src=""http://lwnmusicboosters.org/"" style=""width:1200px; height:1000px; overflow-X:auto; overflow-y:auto;""></iframe>");
+            sb.Append(@" <iframe id=""myiframe"" src=""http://apollo.metal-matic.com/BedfordPark/Conversion/Downtime/Dashboard"" style=""width:1200px; height:1000px; overflow-X:auto; overflow-y:auto;""></iframe>");
             sb.Append(@"</body>");
             sb.Append(@"</html>");
 
             string mysource = sb.ToString();
-            webView1.NavigateToString(mysource);
+            webView2.NavigateToString(mysource);
 
             
         }
@@ -90,8 +183,10 @@ namespace ZumbachPi_V2._0
             //webView1.Width = this.Width;
         }
 
-        private async void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
+
+            LoadWebView(int.Parse(slider.Value.ToString()));
             Debug.WriteLine(slider.Value.ToString());
         //    if (webView1 != null)
         //        await webView1.InvokeScriptAsync("eval", new string[] { "ZoomFunction(" + e.NewValue.ToString() + ");" });
